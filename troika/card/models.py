65e39ef499fbc9d684d3e1aca590c2214dd47479
Pavel Lyashkov <bilbo.kem@gmail.com>
2015-03-29 16:03:59 +0300

    Added model and migration for card

---
+++ troika/card/models.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import datetime as dt
+
+from troika.database import (Column, Model, ReferenceCol, SurrogatePK, db,
+                             relationship)
+
+
+class Card(SurrogatePK, Model):
+
+    __tablename__ = 'cards'
+
+    STATUS_NEW = 'new'
+    STATUS_INPROGRESS = 'inprogress'
+    STATUS_RELEASED = 'released'
+    STATUS_DELIVERED = 'delivered'
+    STATUS_REMOVED = 'removed'
+
+    STATUS_TITLE = {STATUS_NEW: u'Новая',
+                    STATUS_INPROGRESS: u'В процессе',
+                    STATUS_RELEASED: u'Выпущена',
+                    STATUS_DELIVERED: u'Доставлена',
+                    STATUS_REMOVED: u'Удалена'}
+
+    STATE_ACTIVE = 0
+    STATE_LOST = 1
+    STATE_BROKEN = 3
+    STATE_NOT_NEED = 5
+    STATE_REISSUED = 6
+
+    TROIKA_STATE_TITLE = {STATE_ACTIVE: u'Рабочая',
+                          STATE_LOST: u'Утеряна',
+                          STATE_BROKEN: u'Испорчена',
+                          STATE_NOT_NEED: u'Не нужна',
+                          STATE_REISSUED: u'Перевыпущена'}
+
+    hard_id = db.Column(db.String(128), unique=True, nullable=False)
+    troika_id = db.Column(db.String(128), unique=True, nullable=False)
+    name_ru = db.Column(db.String(300))
+    name_en = db.Column(db.String(300))
+    creation_date = db.Column(db.DateTime, nullable=False)
+    troika_state = db.Column(db.Integer(), nullable=False, default=STATUS_NEW)
+    status = db.Column(db.String(128), nullable=False, default=STATE_ACTIVE)
+
+    def __init__(self, **kwargs):
+        db.Model.__init__(self, **kwargs)
+
+    def __repr__(self):
+        return '<Card ({hard_id!r})>'.format(hard_id=self.hard_id)
