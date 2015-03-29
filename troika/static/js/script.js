916e498318972db73f4be217f0d959361d30d0c9
Pavel Lyashkov <bilbo.kem@gmail.com>
2015-03-29 13:37:57 +0300

    Added static files

--- troika/static/js/script.js
+++ troika/static/js/script.js
@@ -1,5 +1,10 @@
 (function($, window) {
+    "use strict";
 
-
-
+    
+  $(document).ready(function() {
+    $(".alert").fadeTo(2000, 500).slideUp(500, function(){
+      $(".alert").alert('close');
+    });
+  });
 }).call(this, jQuery, window);
