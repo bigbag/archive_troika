c6a811c12d5679f7bf3bcabe897b1874cd0dcfa1
Pavel Lyashkov <bilbo.kem@gmail.com>
2015-03-29 13:40:12 +0300

    Update extensions init script

---
+++ troika/extensions.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""Extensions module. Each extension is initialized in the app factory located
+in app.py
+"""
+
+from flask.ext.bcrypt import Bcrypt
+from flask.ext.cache import Cache
+from flask.ext.debugtoolbar import DebugToolbarExtension
+from flask.ext.login import LoginManager
+from flask.ext.migrate import Migrate
+from flask.ext.sqlalchemy import SQLAlchemy
+
+bcrypt = Bcrypt()
+
+login_manager = LoginManager()
+
+db = SQLAlchemy()
+
+migrate = Migrate()
+
+cache = Cache()
+
+debug_toolbar = DebugToolbarExtension()
