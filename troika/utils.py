ac336d7cccdcdd6db83b70111f6afd1b8e3ce3e9
Pavel Lyashkov <bilbo.kem@gmail.com>
2015-03-30 18:07:14 +0300

    Update utils

--- troika/utils.py
+++ troika/utils.py
@@ -7,5 +7,5 @@ def flash_errors(form, category="warning"):
     '''Flash all errors for a form.'''
     for field, errors in form.errors.items():
         for error in errors:
-            flash("{0} - {1}"
-                    .format(getattr(form, field).label.text, error), category)
+            label = getattr(form, field).label.text
+            flash("{0} - {1}".format(label, error), category)
