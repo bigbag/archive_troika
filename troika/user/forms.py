c10d4ebd7cf6b26c4fdfd08a8536aa04f02bec66
Lyashkov Pavel <bilbo.kem@gmail.com>
2015-04-03 16:19:19 +0300

    Update user form

--- troika/user/forms.py
+++ troika/user/forms.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask_wtf import Form
 from wtforms import PasswordField, TextField
 from wtforms.validators import DataRequired, Email, Length
@@ -7,9 +8,14 @@ from .models import User
 
 class LoginForm(Form):
     email = TextField(
-        'Email', validators=[DataRequired(), Email(), Length(min=6, max=40)])
+        'Email',
+        validators=[DataRequired(message=u'Поле обязательно для заполнения'),
+                    Email(),
+                    Length(min=6, max=40)])
     password = PasswordField(
-        'Password', validators=[DataRequired(), Length(min=6, max=40)])
+        'Password',
+        validators=[DataRequired(message=u'Поле обязательно для заполнения'),
+                    Length(min=6, max=40)])
 
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
@@ -22,11 +28,11 @@ class LoginForm(Form):
 
         self.user = User.query.filter_by(email=self.email.data).first()
         if not self.user or not self.user.check_password(self.password.data):
-            self.email.errors.append('User not found')
-            self.password.errors.append('Invalid password')
+            self.email.errors.append(u'Пользователь не найден')
+            self.password.errors.append(u'Пароль не верен')
             return False
 
         if self.user.status != User.STATUS_ACTIVE:
-            self.email.errors.append('User not activated')
+            self.email.errors.append(u'Пользователь не активирован или заблокирован')
             return False
         return True
