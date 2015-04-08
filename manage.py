f67d828198c4cf614fa813dbc66aafd372604fc2
Lyashkov Pavel <bilbo.kem@gmail.com>
2015-04-08 15:31:06 +0300

    Added celery support in startup script

--- manage.py
+++ manage.py
@@ -3,8 +3,10 @@
 import logging
 
 from flask.ext.migrate import MigrateCommand
-from flask.ext.script import Manager, Server, Shell
+from flask.ext.script import Command, Manager, Server, Shell
 
+from celery.app.log import Logging
+from celery.bin.celery import main as celery_main
 from troika.app import create_app
 from troika.card.command import AddCard
 from troika.card.models import Card
@@ -22,6 +24,15 @@ app = create_app(Config)
 manager = Manager(app)
 
 
+class StartCelery(Command):
+
+    def run(self, **args):
+        Logging._setup = True
+        celery_args = ['celery', 'worker', '-B', '-s', '/tmp/celery.db', '--concurrency=2']
+        with app.app_context():
+            return celery_main(celery_args)
+
+
 def _make_context():
     """Return context dict for a shell session so you can access
     app, db, and the User model by default.
@@ -34,6 +45,7 @@ manager.add_command('server', Server(host=app.config['APP_HOST'],
 manager.add_command('shell', Shell(make_context=_make_context))
 manager.add_command('add_user', AddUser)
 manager.add_command('add_card', AddCard)
+manager.add_command('start_celery', StartCelery)
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
