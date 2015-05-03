7510f3b8dc363d905d7a5b8659cdaedc210cc3e1
Lyashkov Pavel <bilbo.kem@gmail.com>
2015-05-03 17:40:05 +0300

    Fixed bugs with gunicorn run

--- troika/__init__.py
+++ troika/__init__.py
@@ -2,6 +2,11 @@
 import logging
 from troika.app import create_app
 
+from troika.card.models import Card
+from troika.history.models import CardsHistory
+from troika.report.models import Report
+from troika.user.models import User
+
 try:
     from troika.settings_local import Config
 except Exception as e:
