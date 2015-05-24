6723964b8ee04eb658557f35cdf00ae9748396a7
Lyashkov Pavel <bilbo.kem@gmail.com>
2015-05-24 23:08:26 +0300

    Update order model

--- troika/order/models.py
+++ troika/order/models.py
@@ -19,7 +19,7 @@ class Order(SurrogatePK, Model):
 
     user_id = db.Column(db.Integer(), nullable=False, unique=True)
     user_email = db.Column(db.String(128), nullable=False, unique=True)
-    image = db.Column(db.String(512), nullable=False)
+    image = db.Column(db.String(512))
     creation_date = db.Column(db.DateTime, nullable=False)
     update_date = db.Column(db.DateTime, default=date_helper.get_current_date())
     status = db.Column(db.String(128), nullable=False, default=STATUS_NEW)
